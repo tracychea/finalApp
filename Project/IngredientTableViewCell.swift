1df14383229e0dac9c8ae8cff00610daa1a7e641
Tracy <tracychea@utexas.edu>
2018-05-01 00:06:19 -0500

    Sarah's Part

--- Project/IngredientTableViewCell.swift
+++ Project/IngredientTableViewCell.swift
@@ -12,6 +12,44 @@ class IngredientTableViewCell: UITableViewCell {
     
     @IBOutlet weak var ingredientList: UILabel!
     
+    @IBOutlet weak var unCheck: UIButton!
+    //var checked = false
+    @IBOutlet weak var Check: UIButton!
+    
+    
+    func setButtons()
+    {
+        unCheck.isHidden = true
+    }
+    
+    @IBAction func CheckButton(sender: AnyObject)
+    {
+        Check.isHidden = true
+        unCheck.isHidden = false
+        
+        //do other stuff
+    }
+    
+    @IBAction func unCheckButton(sender: AnyObject)
+    {
+        Check.isHidden = false
+        unCheck.isHidden = true
+        
+        //do other stuff
+    }
+    // @IBAction func button(sender: UIButton) {
+    
+    // button.setImage(UIImage(named: "myImage.png")!, forState: .Normal)
+    // button.setImage(UIImage(named: "checkBox.png")!, for: .selected)
+    //button.setImage(UIImage(named: "checkBox"), for: .selected)
+    // if checked == false {
+    //      button.setImage(UIImage(named:"box"), for: .normal)
+    //       checked = true
+    //  } else {
+    //        button.setImage(UIImage(named:"checkBox"), for: .normal)
+    //        checked = false
+    //    }
+    //}
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -22,5 +60,6 @@ class IngredientTableViewCell: UITableViewCell {
         
         // Configure the view for the selected state
     }
-
+    
 }
+
--- Project/IngredientTableViewController.swift
+++ Project/IngredientTableViewController.swift
@@ -7,29 +7,52 @@
 //
 
 import UIKit
-
+import CoreData
 class IngredientTableViewController: UITableViewController {
     
-    private var Ingredientlist: [ingredientList] = []
+   // private var Ingredientlist: [ingredientList] = []
+    var ingredient = [NSManagedObject]()
     
+    //weak var checkImage: UIImageView! //{box.png}
+    
+    
+    
+    
+    var itemToDelete = String()
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = settingService.sharedService.backgroundColor;
-
+        //var image : UIImage = UIImage(named:"box")!
+        //checkImage = UIImageView(image: image)
+        
         
     }
     
-    private func createDataModel() {
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName:"Ingredient")
+        var fetchedResults:[NSManagedObject]? = nil
         
-        Ingredientlist.append(ingredientList(ingredient: "carrots"))
-        Ingredientlist.append(ingredientList(ingredient: "mushrooms"))
-        Ingredientlist.append(ingredientList(ingredient: "cheese"))
-        Ingredientlist.append(ingredientList(ingredient: "chicken breast"))
-        Ingredientlist.append(ingredientList(ingredient: "steak"))
-        Ingredientlist.append(ingredientList(ingredient: "pasta"))
-        Ingredientlist.append(ingredientList(ingredient: "olive oil"))
-        Ingredientlist.append(ingredientList(ingredient: "bell pepper"))
+        // Try and get data
+        do {
+            try fetchedResults = managedContext.fetch(fetchRequest) as? [NSManagedObject]
+        } catch {
+            // what to do if an error occurs?
+            let nserror = error as NSError
+            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+        }
         
+        // Set data to local variable
+        if let results = fetchedResults {
+            ingredient = results
+        } else {
+            print("Could not fetch")
+        }
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -43,7 +66,7 @@ class IngredientTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return self.Ingredientlist.count
+        return self.ingredient.count
     }
     
     
@@ -56,58 +79,130 @@ class IngredientTableViewController: UITableViewController {
         }
         
         // Configure the cell...
-        let ingredientlists = Ingredientlist[indexPath.row]
+        let ingredientlists = ingredient[indexPath.row]
         
-        cell.ingredientList.text = ingredientlists.ingredient
+        cell.ingredientList.text = ingredientlists.value(forKey: "ingredient") as? String
+        //itemToDelete = (ingredientlists.value(forKey: "ingredient") as? String)!
         return cell
+        
+    }
+    
+    @IBAction func deleteIngredient (_ingredientList:Any) {
+        self.deleteFromCoreData()
+        DispatchQueue.main.async {
+            self.tableView!.reloadData()
+            //self.tableView.reloadRowsAtIndexPaths( withRowAnimation: UITableViewRowAnimation.None)
+            //      self.IngredientTableViewController.reloadData()
+            // let rowNumber: Int = 0
+            //let sectionNumber: Int = 0
+            
+            //let indexPath = IndexPath(item: rowNumber, section: sectionNumber)
+            
+            //self.tableView.reloadRows(at: [indexPath], with: .automatic)
+            
+        }
+    }
+    func getContext () -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
     }
     
     
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
     
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
+    func deleteFromCoreData() -> Void {
+        //  let indexPath = tableView.indexPathForSelectedRow
+        let moc = getContext()
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Ingredient")
+        //  fetchRequest.predicate = NSPredicate(format: "movieTitle == %@")
+        //let fetchRequest.predicate = NSPredicate(forKey: "ingredient", itemToDelete)
+        
+        let result = try? moc.fetch(fetchRequest)
+        let resultData = result as! [Ingredient]
+        
+        for object in resultData {
+            moc.delete(object)
+            //  moc.delete(fetchRequest[indexPath.row])
+        }
+        
+        do {
+            try moc.save()
+            print("saved!")
+        } catch let error as NSError  {
+            print("Could not save \(error), \(error.userInfo)")
+        } catch {
+            
+        }
+        
+    }
     
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+    
+    
+    /*    let indexPath = tableView.indexPathForSelectedRow
+     // let selectedRow =  indexPath?.row
+     guard let cell = tableView.dequeueReusableCell(withIdentifier: "IngredientTableViewCell", for: indexPath) as?
+     IngredientTableViewCell else {
+     fatalError("The dequeued cell is not an instance of TableViewCell")
      
      }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
      
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
+     let grocery = cell.ingredientList.text
+     //  let grocery = ingredientList.self
+     self.SaveIngredient(grocery.text)
      */
+    
+    
+    //   }
+    /*   func SaveIngredient (_grocery:AnyObject) {
+     let appDelegate = UIApplication.shared.delegate as! AppDelegate
+     let managedContext = appDelegate.persistentContainer.viewContext
+     let entity = NSEntityDescription.entity(forEntityName: "Ingredients", in: managedContext)
+     let ingredient = NSManagedObject(entity: entity!, insertInto: managedContext)
+     
+     
+     //print(grocery)
+     
+     ingredient.setValue(grocery, forKey: "item")
+     
+     do {
+     try managedContext.save()
+     } catch {
+     // what to do if an error occurs?
+     let nserror = error as NSError
+     NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+     abort()
+     }
+     
+     ingredients.append(ingredient)
+     }*/
+    // if let indexPath = tableView.indexPathForSelectedRow {
+    //   let selectedRow =  indexPath.row
+    //   let destinationVC = segue.destination as! ListTableViewController
+    //   destinationVC.ingredients = self.Ingredientlist[selectedRow]
+    
+    
+    
+    //            GroceryList.append(GroceryList(ingredient:self.Ingredientlist[selectedRow]))
+    //let cellIdentifier = "IngredientTableViewCell"
+    //   let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for cellForRowAt indexPath)
+    // let Ingredient = Ingredientlist.ingredient.text!
+    // self.saveIngredient(Ingredient)
+    
+    
+    //  func prepare(for segue: UIStoryboardSegue,  sender: Any?) {
+    //    if let indexPath = tableView.indexPathForSelectedRow {
+    //         let selectedRow =  indexPath.row
+    //        let destinationVC = segue.destination as! ListTableViewController
+    //        destinationVC.ingredient = self.Ingredientlist[selectedRow]
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    
+    
+    
+    
+    
+    
+    
     
 }
-
 
